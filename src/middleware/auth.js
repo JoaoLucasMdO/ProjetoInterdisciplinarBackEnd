71c6dd5eeab3faf775080f5dd794e851b1ec2f20
João Lucas Melo de Oliveira <joao.oliveira307@fatec.sp.gov.br>
2024-06-10 16:58:51 -0300

    Att

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -3,7 +3,7 @@ const jwt = require('jsonwebtoken')
 
 exports.autenticar = async function auth(req, res, next){
     const token = req.header('access-token')
-    if(!token) return res.status(401).json({ //401-Not Authorized
+    if(!token) res.status(401).json({ //401-Not Authorized
         msg: 'Acesso negado. É obrigatório o envio do token JWT'
     })
     try {
@@ -16,7 +16,7 @@ exports.autenticar = async function auth(req, res, next){
        req.usuario = await decoded.usuario
        next() // Direcionamos para o endpoint
     }catch(e){
-        res.status(403).send({error: `Token inválido: ${e.message}`})
+        res.status(400).send({error: `Token inválido: ${e.message}`})
     }
 }
 

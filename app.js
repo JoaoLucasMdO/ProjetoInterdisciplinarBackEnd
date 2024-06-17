d87be2dd5a526e7a4d5471a977e80d639b4163b5
Gesley de Oliveira Rosa <127444907+GesleyOliveira@users.noreply.github.com>
2024-06-17 17:06:55 -0300

    att

--- app.js
+++ app.js
@@ -14,15 +14,6 @@ const usuarioRoute = require('./src/routes/usuarioRoutes')
 const materiaRoute = require('./src/routes/materiaRoutes')
 const horarioRoute = require('./src/routes/horarioRoutes')
 
-//Conexão do Banco
-//Colocar url pessoal aqui e comentar a debaixo!
-let url = 'mongodb+srv://janmello123:joao123@projetointerdisciplinar.yuod8po.mongodb.net/projetoInterdisciplinar';
-//let url = 'mongodb://127.0.0.1:27017/projetoInterdisciplinar';
-mongoose.connect(url);
-mongoose.Promise = global.Promise;
-let db = mongoose.connection;
-db.on('error', console.error.bind(console, 'Erro ao conectar ao MongoDB'));
-
 //Uses
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: false}))
@@ -39,9 +30,8 @@ app.set('views', './src/views')
 
 //Config
 config.config()
-
-//Conexão do Banco
 let db = mongoose.connection;
+//Conexão do Banco
 //Colocar url pessoal aqui e comentar a debaixo!
 //let url = 'mongodb+srv://janmello123:joao123@projetointerdisciplinar.yuod8po.mongodb.net/projetoInterdisciplinar';
 let url = 'mongodb+srv://GesleyRosa:Gelao*123@fatec.8bwsw58.mongodb.net';
